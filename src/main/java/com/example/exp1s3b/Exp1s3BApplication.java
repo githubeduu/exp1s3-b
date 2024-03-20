26624fcead1ccbfc773be474599ac1b07339ca88
Eduardo <edo.carvajal@hotmail.com>
2024-03-20 17:41:43 -0300

    first commit

---
+++ src/main/java/com/example/exp1s3b/Exp1s3BApplication.java
@@ -0,0 +1,13 @@
+package com.example.exp1s3b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp1s3BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp1s3BApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exp1s3b/Exp1s3BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exp1s3b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp1s3BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
